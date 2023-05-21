ed469df94196235f07ae2f2736aeffa009f19f9b
yuliya <liana-yuliana@list.ru>
2023-05-21 23:00:31 +0300

    Added Task008_AllEven

---
+++ Task008_AllEven/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные от 1 до N
+
+int count = 1;
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+while (count <= N)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write(count + " ");
+    }
+    count++;
+}
