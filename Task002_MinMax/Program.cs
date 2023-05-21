5a2e9973411b24e51cd4f30150aa07aa45b11387
yuliya <liana-yuliana@list.ru>
2023-05-21 16:27:18 +0300

    Added Task002

---
+++ Task002_MinMax/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает два числа 
+// и выдает, какое число большее, а какое меньшее
+
+Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.Write ("max = ");
+    Console.WriteLine (numberA);
+    Console.Write ("min = ");
+    Console.WriteLine (numberB);
+}
+else
+{
+    Console.Write ("max = ");
+    Console.WriteLine (numberB);
+    Console.Write ("min = ");
+    Console.WriteLine (numberA);
+}
