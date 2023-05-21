a987ce64633c8837654d2d713b304917beecc887
yuliya <liana-yuliana@list.ru>
2023-05-21 22:47:17 +0300

    Added Task006_Even

---
+++ Task004_Max/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает три числа 
+// и выдает максимальное из этих чисел
+
+Console.WriteLine("Введите три числа:");
+int numberA = int.Parse(Console.ReadLine());
+int numberB = int.Parse(Console.ReadLine());
+int numberC = int.Parse(Console.ReadLine());
+
+int max = numberA;
+
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+
+Console.Write ("Максимальное число ");
+Console.WriteLine (max);
---
+++ Task006_Even/Program.cs
@@ -0,0 +1,14 @@
+﻿// // Напишите программу, которая на вход принимает число 
+// и выдает, является ли число чётным
+
+Console.Write ("Число ");
+int number = new Random().Next(-100,100);
+Console.Write (number + " ");
+if (number % 2 == 0)
+{
+    Console.Write ("Чётное");
+}
+else
+{
+    Console.Write ("Нечётное");
+}
